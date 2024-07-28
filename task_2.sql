442b0649f3d19120f4cac62aa37a258f6671f1bb
Paulose <pthiopia@outlook.com>
2024-07-28 08:59:57 +0300

    task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -30,7 +30,7 @@ CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table
