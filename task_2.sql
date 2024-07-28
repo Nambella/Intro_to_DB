0eca9724bff667ccecff5ff0c22afbb7041471da
Paulose <pthiopia@outlook.com>
2024-07-28 08:53:41 +0300

    task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,44 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Create the Books table
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Create the Authors table
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+-- Create the Customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- Create the Orders table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create the Order_Details table
+CREATE TABLE order_details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
