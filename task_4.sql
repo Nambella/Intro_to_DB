daf50acf2e9935b58cc6d6a64d0e256c45562580
Paulose <pthiopia@outlook.com>
2024-07-28 12:50:50 +0300

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
 -- Prints the full description of the table books in my MySQL server
 SHOW CREATE TABLE `books`;
+-- Prints the full description of the table Books in my MySQL server
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
